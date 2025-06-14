f4441f5fb4fbd189cac25143888ad8cccbfed7a4
Yavor Petrov <petrovkunev@gmail.com>
2025-06-14 23:36:05 +0300

    Initial Architecture

---
+++ FlowKunevDev/FlowKunevDev.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlowKunevDev.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
