dd84b486b4e83bb64821d5ac668729d84cf8bc5f
Yavor Petrov <petrovkunev@gmail.com>
2025-06-29 01:51:42 +0300

    Add calculator feature to Home section
    Introduced a new calculator view and action in HomeController, allowing users to manage incomes and expenses interactively. Updated the main layout to include a navigation link to the calculator page.

--- FlowKunevDev/FlowKunevDev.Web/Controllers/HomeController.cs
+++ FlowKunevDev/FlowKunevDev.Web/Controllers/HomeController.cs
@@ -35,6 +35,11 @@ namespace FlowKunevDev.Web.Controllers
             return View();
         }
 
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
