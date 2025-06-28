4d341f53f2cc8576ee84ed01ce8b1501c8d44f9b
Yavor Petrov <147103195+PetrovKunev@users.noreply.github.com>
2025-06-28 15:03:05 +0300

    Remove seconds from DateTime values in TimeHelper
    Updated TimeHelper to strip seconds from all returned DateTime values by introducing a RemoveSeconds method. This ensures consistency in time precision across UtcNow, LocalNow, and ConvertUtcToLocal.

--- FlowKunevDev/FlowKunevDev.Common/TimeHelper.cs
+++ FlowKunevDev/FlowKunevDev.Common/TimeHelper.cs
@@ -6,9 +6,9 @@ namespace FlowKunevDev.Common
     {
         private static readonly string[] SofiaTimeZoneIds = { "FLE Standard Time", "Europe/Sofia" };
 
-        public static DateTime UtcNow => DateTime.UtcNow;
+        public static DateTime UtcNow => RemoveSeconds(DateTime.UtcNow);
 
-        public static DateTime LocalNow => ConvertUtcToLocal(DateTime.UtcNow);
+        public static DateTime LocalNow => RemoveSeconds(ConvertUtcToLocal(DateTime.UtcNow));
 
         public static DateTime ConvertUtcToLocal(DateTime utcDate)
         {
@@ -17,12 +17,24 @@ namespace FlowKunevDev.Common
                 try
                 {
                     var tz = TimeZoneInfo.FindSystemTimeZoneById(id);
-                    return TimeZoneInfo.ConvertTimeFromUtc(DateTime.SpecifyKind(utcDate, DateTimeKind.Utc), tz);
+                    return RemoveSeconds(TimeZoneInfo.ConvertTimeFromUtc(DateTime.SpecifyKind(utcDate, DateTimeKind.Utc), tz));
                 }
                 catch (TimeZoneNotFoundException) { }
                 catch (InvalidTimeZoneException) { }
             }
-            return utcDate;
+            return RemoveSeconds(utcDate);
+        }
+
+        private static DateTime RemoveSeconds(DateTime dateTime)
+        {
+            return new DateTime(
+                dateTime.Year,
+                dateTime.Month,
+                dateTime.Day,
+                dateTime.Hour,
+                dateTime.Minute,
+                0,
+                dateTime.Kind);
         }
     }
 }
